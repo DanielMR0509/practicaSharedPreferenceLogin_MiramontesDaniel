f4215a1d511379aa6868d471bea9dc58769c12c2
DanielMR0509 <daniel.miramontes252801@potros.itson.edu.mx>
2026-03-24 19:31:48 -0700

    Avance en uso de DataStore.

---
+++ app/src/main/java/mx/edu/itson/practicasharedpreferencelogin_miramontesdaniel/data/DataStoreManager.kt
@@ -0,0 +1,31 @@
+package mx.edu.itson.practicasharedpreferencelogin_miramontesdaniel.data
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class DataStoreManager(private val context: Context) {
+    private val Context.dataStore by preferencesDataStore("session_prefs")
+
+    companion object {
+        val IS_LOGGED_IN = booleanPreferencesKey("is_logged_in")
+    }
+
+    val isLoggedFlow: Flow<Boolean> = context.dataStore.data
+        .map { it[IS_LOGGED_IN] ?:false }
+
+    suspend fun logout() {
+        context.dataStore.edit {
+            it.clear()
+        }
+    }
+
+    suspend fun setLogin(isLoggedIn: Boolean) {
+        context.dataStore.edit {
+            it[IS_LOGGED_IN] = isLoggedIn
+        }
+    }
+}
